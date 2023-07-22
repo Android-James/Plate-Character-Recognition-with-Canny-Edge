842c59027f0c1a8e6d5598366e846b2b317a4bae
blancojames88 <blancojames88@gmail.com>
2023-07-22 11:33:36 +0800

    refactored file paths

--- Plate Character Detection/main.py
+++ Plate Character Detection/main.py
@@ -6,12 +6,12 @@ results = {}
 
 # load models
 #model1 (pre-trained on COCO2017 for cars detection)
-coco_model = YOLO('yolov8n.pt') 
+coco_model = YOLO('C:\YOLOv8 and Canny Edge\Plate-Character-Recognition-with-Canny-Edge\yolov8n.pt') 
 
 #model2 (pre-trained on license plates dataset for license plates detection)
 
 # load video
-cap = cv2.VideoCapture('/')
+cap = cv2.VideoCapture('C:\YOLOv8 and Canny Edge\Plate-Character-Recognition-with-Canny-Edge\license_dataset\\videos\\testVideo5.mp4')
 
 vehicles = [2, 3, 5, 7]
 
@@ -27,19 +27,12 @@ while ret:
         detections = coco_model(frame)[0]
         detections_ = []
         for detection in detections.boxes.data.tolist():
+            # print(detection)
             x1, y1, x2, y2, score, class_id = detection
             if int(class_id) in vehicles:
                 detections_.append([x1, y1, x2, y2, score])
+                print(detections_)
 
-
-
-# load video
-
-
-
-# read frames
-
-# detect vehicles
        
 # track vehicles
 
