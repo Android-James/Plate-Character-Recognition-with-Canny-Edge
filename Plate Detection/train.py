9769755f1da65b28f71a949e24eba4e47e5e51c2
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-07-22 14:02:37 +0800

    mat train

--- Plate Detection/train.py
+++ Plate Detection/train.py
@@ -1,7 +1,26 @@
+import torch
 from ultralytics import YOLO
 
+    
+
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+torch.cuda.empty_cache()
+print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
+def run():
+    torch.multiprocessing.freeze_support()
+    #Use the model
+    results = model.train(data="Plate Detection\config.yaml", epochs=1, batch=3)  # train the model
 
-# Use the model
-results = model.train(data="Plate Detection\config.yaml", epochs=1)  # train the model
+if __name__ == '__main__':
+    run()    
+
+
+
+
+
+
+
