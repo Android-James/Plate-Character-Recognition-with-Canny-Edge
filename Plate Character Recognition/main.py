c2f1fa9da6dc8943336f31b5e07eaff895fd75c7
ImperialDuck-dev <mjmuyco13@gmail.com>
2023-08-05 21:00:00 +0800

    done with video testing

--- Plate Character Recognition/main.py
+++ Plate Character Recognition/main.py
@@ -17,7 +17,7 @@ coco_model = YOLO('yolov8n.pt')
 license_plate_detector = YOLO('D:\Plate-Character-Recognition-with-Canny-Edge\\runs\\detect\\train6\\weights\\last.pt')
 
 # load video
-cap = cv2.VideoCapture('D:\Plate-Character-Recognition-with-Canny-Edge\\license_dataset\\videos\\danica dataset\\testVideo4k-30.MOV')
+cap = cv2.VideoCapture('D:\Plate-Character-Recognition-with-Canny-Edge\\license_dataset\\videos\\danica dataset\\testVideo4k-30-2.MOV')
 
 vehicles = [2,3,6,8]
 
@@ -86,4 +86,4 @@ while ret:
                                                                     'text_score': license_plate_text_score}}
 
 #  write results
-write_csv(results, './testVideo4k-30-canny.csv')
+write_csv(results, './testVideo4k-30-2-canny.csv')
